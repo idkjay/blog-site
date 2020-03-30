2530f6852751bc1ed7c08b6903d5844bb559a2cc
Jay <jordanchu95@gmail.com>
2020-03-30 17:52:17 -0400

    edit button on index

--- routes/articles.js
+++ routes/articles.js
@@ -22,9 +22,10 @@ router.post('/', async (req, res, next) => {
    next()
 }, saveArticleAndRedirect('new'))
 
-router.put('/:id', (req,res) => {
-
-})
+router.put('/:id', async (req, res, next) => {
+   req.article = await Article.findById(req.params.id)
+   next()
+}, saveArticleAndRedirect('edit'))
 
 router.delete('/:id', async (req, res) => {
     await Article.findByIdAndDelete(req.params.id)
@@ -32,7 +33,7 @@ router.delete('/:id', async (req, res) => {
 })
 
 function saveArticleAndRedirect(path) {
-    return (req, res) => {
+    return async (req, res) => {
         let article = req.article 
         article.title = req.body.title
         article.description = req.body.description
