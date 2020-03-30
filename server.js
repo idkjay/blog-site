76ba9737bad97415a7d1eab8d183c0e513765c13
Jay <jordanchu95@gmail.com>
2020-03-30 16:47:41 -0400

    configuring mongodb

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
 const app = express()
 
+mongoose.connect('mongodb://localhost/blog', {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
+
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
 
-app.use('/articles', articleRouter)
-
-app.get('/', (req, res) => {
-    const articles = [{
-        title: 'Test Article',
-        createdAt: new Date(),
-        description: 'Test Description'
-    },
-    {
-        title: 'Test Article2',
-        createdAt: new Date(),
-        description: 'Test Description2'
-    }]
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({
+        createdAt: 'desc'
+    })
     res.render('articles/index', { articles: articles })
 })
+
+app.use('/articles', articleRouter)
 
 app.listen(5000)
